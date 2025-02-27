8d89021afbc8240c02ca7b87c5c394f70d93c644
OriBerger <oriberger12@gmail.com>
2025-02-27 17:50:49 +0200

    update 8

--- server/src/server.ts
+++ server/src/server.ts
@@ -30,7 +30,6 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use("/api", codeBlockRoutes);
-app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
